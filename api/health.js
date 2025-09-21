9016bd8b9b5e2601862c699d03a67975ddad2aed
stvmachine-kaluza <esteban.valenzuela@kaluza.com>
2025-09-22 09:43:44 +1000

    refactor: removed api folder

--- api/health.js
+++
@@ -1,6 +0,0 @@
-// This file is deprecated - use dist/handler.js instead
-// The Vercel configuration now points to the built handler in dist/
-
-const handler = require('../dist/handler.js').default;
-
-module.exports = handler;
